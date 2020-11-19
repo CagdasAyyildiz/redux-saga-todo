9c264a25bf9151a5799aa88946de190914faf69d
CagdasAyyildiz <cagdasayyildiz.ayyildiz@gmail.com>
2020-11-19 15:50:18 +0300

    async fetch add

--- src/store/sagas/workers.js
+++ src/store/sagas/workers.js
@@ -1,9 +1,13 @@
-import { put } from 'redux-saga/effects';
+import { put,call  } from 'redux-saga/effects';
 import actions from '../actions/actions';
 
+const delay = time => new Promise(resolve => setTimeout(resolve, time));
+
+
 export function* fetchTodosSaga() {
-    const json_file = yield fetch("todos.json")
+    const json_file = yield fetch("todos.json");
     const todos = yield json_file.json();
-    yield put({type: actions.FETCH_TODOS_SUCCEED, todos: todos.todos})
+    yield call(delay,2000);
+    yield put({type: actions.FETCH_TODOS_SUCCEED, todos: todos.todos});
 }
 
